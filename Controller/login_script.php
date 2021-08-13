98a72990d46b3b38ac585ad3cab7db4e7de2a587
SVilcaLim03 <66433673+SVilcaLim03@users.noreply.github.com>
2021-08-12 20:24:38 -0500

    Add files via upload

--- Controller/login_script.php
+++ Controller/login_script.php
@@ -11,49 +11,30 @@
     $_SESSION["username"] = "$username";
     
        
-    $query1 = "SELECT id_cliente FROM cliente_usuario WHERE username='$username' AND password='$password'";
-    $query2 = "SELECT id_personal FROM personal WHERE username='$username' AND password='$password'";
-    $query3 = "SELECT id_cargo FROM personal WHERE username='$username' AND password='$password'";
+    $query1 = "SELECT id_personal FROM personal WHERE username='$username' AND password='$password'";
+    $query2 = "SELECT id_cliente FROM cliente_usuario WHERE username='$username' AND password='$password'";
 
 	$result1=mysqli_query($conn, $query1);
     $result2=mysqli_query($conn, $query2);
-    $result3=mysqli_query($conn, $query3);
-
-	if(mysqli_num_rows($result1) > 0)
+    
+    if(mysqli_num_rows($result1) > 0)
 	{
-        echo 0;
-        while ($registro = mysqli_fetch_array($result1))
-        {
-            $id = $registro["id"];
-            $_SESSION["id"] = "$id";
-        }
-        $_SESSION["permisos"] = "user_employer";
-    }
-    else if (mysqli_num_rows($result2) > 0 and mysqli_fetch_field_direct($result3, 0) != 1)
-    {
-        echo 1;
-        while ($registro = mysqli_fetch_array($result2))
-        {
-            $id = $registro["id"];
-            $_SESSION["id"] = "$id";
-        }
-        $_SESSION["permisos"] = "user_client";
-    }
-    else if (mysqli_fetch_field_direct($result3, 0) == 1)
-    {
+        $rol = 2;
+        $_SESSION['rol'] = $rol;
         echo 2;
-        while ($registro = mysqli_fetch_array($result3))
-        {
-            $id = $registro["id"];
-            $_SESSION["id"] = "$id";
-        }
-        $_SESSION["permisos"] = "user_client";
+    }
+        
+    else if (mysqli_num_rows($result2) > 0)
+    {
+        $rol = 0;
+        $_SESSION['rol'] = $rol;
+        echo 0;
     }
     else
     {
         echo 3;
     }
-
+    
     $connection->CloseConnection();
     exit;
 ?>
